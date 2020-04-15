6779bfa8bce905e7ae9fff5edf02a6e3a068801e
Jack魏 <1960486253@qq.com>
2020-04-15 21:25:43 +0800

    演示demo界面2

--- src/main/java/com/phms/controller/open/LoginController.java
+++ src/main/java/com/phms/controller/open/LoginController.java
@@ -61,6 +61,22 @@ public class LoginController {
 		return "tu";
 	}
 
+
+	@RequestMapping(value = "/demo/tu1", method = RequestMethod.GET)
+	public String tu1() {
+		return "tu1";
+	}
+
+	@RequestMapping(value = "/demo/tu2", method = RequestMethod.GET)
+	public String tu2() {
+		return "tu2";
+	}
+
+	@RequestMapping(value = "/demo/tu3", method = RequestMethod.GET)
+	public String tu3() {
+		return "tu3";
+	}
+
 	/**
 	 * Method name: logout <BR>
 	 * Description: 退出登录 <BR>
