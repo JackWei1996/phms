585f6936d1dd8b32779d8af1d04237ed8cba6cdf
Jack魏 <1960486253@qq.com>
2020-04-27 22:25:15 +0800

    图片上传

--- src/main/java/com/phms/controller/open/UploadFileController.java
+++ src/main/java/com/phms/controller/open/UploadFileController.java
@@ -62,4 +62,41 @@ public class UploadFileController {
             out.println(s);
         }
     }
+
+    @RequestMapping(value = "/layuiUpload", method = RequestMethod.POST)
+    @ResponseBody
+    public void layuiUpload(MultipartFile file,
+                            HttpServletRequest request, HttpServletResponse response){
+        if (file.isEmpty()) {
+            System.out.println("文件为空空");
+        }
+        PrintWriter out=null;
+        // CKEditor提交的很重要的一个参数 ,回调函数的序号
+        String callback = request.getParameter("CKEditorFuncNum");
+
+
+        String fileName = file.getOriginalFilename();  // 文件名
+        String suffixName = fileName.substring(fileName.lastIndexOf("."));  // 后缀名
+        String filePath = "D://upload//"; // 上传后的路径
+        fileName = UUID.randomUUID() + suffixName; // 新文件名
+
+        FileResponse fileResponse = new FileResponse();
+        File dest = new File(filePath + fileName);
+
+        if (!dest.getParentFile().exists()) {
+            dest.getParentFile().mkdirs();
+        }
+        try {
+            file.transferTo(dest);
+            out = response.getWriter();
+            String filename = "/file/" + fileName;
+            String s = fileResponse.success(1, fileName,  filename, null);
+            // 返回"图像"选项卡并显示图片  request.getContextPath()为web项目名
+            out.println(s);
+        } catch (IOException e) {
+            String s = fileResponse.error(0, "上传失败！");
+            // 返回"图像"选项卡并显示图片  request.getContextPath()为web项目名
+            out.println(s);
+        }
+    }
 }
